19224fda95a2fa5caac932412fecc661eac2320a
LukaKoridze13 <lukakoridze13@gmail.com>
2023-03-07 22:44:54 +0400

    Cors Problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect } from "react";
+import { io } from "socket.io-client";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const socket = io("http://localhost:3000", {
+      withCredentials: true,
+      extraHeaders: {
+        "Access-Control-Allow-Origin": "http://localhost:5173",
+      },
+    });
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    socket.on("connect", () => {
+      console.log("connected to server");
+    });
+
+    socket.on("chat message", (msg) => {
+      console.log("message: " + msg);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("disconnected from server");
+    });
+
+    // send a message to the server
+    socket.emit("chat message", "Hello, server!");
+  }, []);
+
+  return <>Div</>;
 }
 
-export default App
+export default App;
